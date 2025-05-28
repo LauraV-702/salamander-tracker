c6281a3fafbad2dbb052009edaec666ffaaaf8a8
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-05-28 14:16:59 -0700

    Added a button to go back to the Videos page

--- src/components/PreviewPage.jsx
+++ src/components/PreviewPage.jsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function PreviewPage({ filename }) {
   return (
     <div>
-      <h1 style={{ marginBottom: 25 }}>Previewing: {filename}</h1>
+      <h1 style={{ marginBottom: 25, marginLeft: 20 }}>Previewing: {filename}</h1>
       <video src={`/videos/${filename}`} controls width="600" />
+      <div style={{ marginLeft: 30 }}>
+        <Link style={{ backgroundColor: "green", padding: 20}} href="/videos">Back to Videos</Link>
+      </div>
     </div>
   );
 }
